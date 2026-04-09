34336d6f01659e08fa7933eac55557f62f99f9d0
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-04-09 11:25:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const colorPicker = document.querySelector('#color-picker');
+const fontSelect = document.querySelector('#font-select');
+const themeToggle = document.querySelector('#theme-toggle');
+const articleColorPicker = document.querySelector('#article-color-picker');
+const body = document.querySelector('body');
+const newsArticle = document.querySelectorAll('.news');
+
+// carrega as preferencias do usuario armazenadas no localStorage
+
+const loadPreferences = () => {
+    //cor
+
+};
+
+colorPicker.addEventListener('input', (e) => {
+    const color = e.target.value; //capturar o movimento do mouse
+    document.documentElement.style.setProperty('--primary-color', color); //mudar a cor do site
+    //inserir a chave (primaryColor) e o valor (color) no localStorage
+    localStorage.setItem('primaryColor', color);
+});
+
+articleColorPicker.addEventListener('input', (e) => {
+    const color = e.target.value;
+    newsArticle.forEach(article => {
+        article.style.backgroundColor = color;
+});
+localStorage.setItem('articleColor', color);
+});
+
+fontSelect.addEventListener('change', (e) => {});
+
+themeToggle.addEventListener('click', () => {});

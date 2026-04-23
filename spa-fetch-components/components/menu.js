54f0fc0180326aa56e7c72542cfda40a79bb26ee
Gabrielli Michachi <gabrielli.paula@aluno.senai.br>
2026-04-23 10:33:45 -0300

    começamos o spa

---
+++ spa-fetch-components/components/menu.js
@@ -0,0 +1,24 @@
+// inicializa comportamento do menu mobile //
+    export const iniciarMenu = () =>{
+        const menus = document.querySelectorAll('[data-menu]')
+
+        menus.forEach((menu) =>{
+        
+        const botao = menu.querySelector('.menu__botao');
+
+        const toggleMenu = () => {
+
+            menu.classList.toggle('active');
+
+            const ativo = menu.classList.contains('active');
+
+            botao.setAttribute('aria-expanded', ativo)
+            botao.setAttribute('aria-label', ativo ? 'fechar menu': 'abrir menu')
+        };
+
+        const fecharComEsc = (event) => {
+            const isEsc = event.key === 'Escape';
+            const ativo = menu.classList.contains('active');
+        };
+    });
+};
---
+++ spa-fetch-components/js/main.js
@@ -0,0 +1,1 @@
+import { iniciarMenu } from "../components/menu.js";

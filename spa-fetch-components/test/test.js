2cc74d4dbb0a8c79b40f5dd206613444b9515a75
Gabrielli Michachi <gabrielli.paula@aluno.senai.br>
2026-04-24 14:58:50 -0300

    terminamos test

--- spa-fetch-components/test/test.js
+++ spa-fetch-components/test/test.js
@@ -1,12 +1,20 @@
-try {
-   
-    // tratar erros //
-}catch(erro){
-    let text = `
-    <strong>Nome do erro:</strong> ${erro.name} <br />
-    <strong>Mensagem:</strong> ${erro.message}  <br />
-    <strong>Stack:</strong> <span>${erro.stack} </span>
-    `;
+const carregarComponente = async (caminho, container) => {
+    try{
 
-    document.body.innerHTML = text
+        const resposta = await fetch(caminho);
+
+        if (!resposta.ok){
+
+            throw new Error('Erro ao carregar componente')
+        }
+            
+        const html = await resposta.text();
+
+        container.innerHTML = html;
+
+    }catch(erro){
+        console.error(erro);
+
+    }
+
 };
